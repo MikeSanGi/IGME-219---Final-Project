d67c41a84dbd1674fdf8db81db7a67d4cfd9140a
Michael SanGiovanni <msangiovanni@snet.net>
2016-12-15 00:17:02 -0500

    Added new Drawbridge + Inner Wall.  Flags now loop animation.  Drawbridge animated upon click, though still working out some bugs on that one.

---
+++ UnityProject (Latest)/Assets/Scripts/DrawbridgeAnim.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DrawbridgeAnim : MonoBehaviour 
+{
+	bool up = false;
+	void Update()
+	{
+		if (Input.GetMouseButtonDown (0))
+		{
+			if(!up)
+			{
+				gameObject.GetComponent<Animation>()["Take 001"].speed = 1.0f; 
+				gameObject.GetComponent<Animation> ().Play ("Take 001");
+				Debug.Log ("Drawbridge Clicked");
+				up = true;
+			}
+			else if(up)
+			{
+				gameObject.GetComponent<Animation>()["Take 001"].speed = - 1.0f;
+				gameObject.GetComponent<Animation> ().Play ("Take 001");
+				Debug.Log ("Drawbridge Clicked");
+				up = false;
+			}
+		}
+	}
+}
