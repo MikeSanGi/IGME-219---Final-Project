ac3dd16a387da1178bd402f2605d2e9644398b71
Michael SanGiovanni <msangiovanni@snet.net>
2016-12-15 14:00:26 -0500

    Fixed a scripting issue on my drawbridge

--- UnityProject (Latest)/Assets/Scripts/DrawbridgeAnim.cs
+++ UnityProject (Latest)/Assets/Scripts/DrawbridgeAnim.cs
@@ -4,24 +4,20 @@ using System.Collections;
 public class DrawbridgeAnim : MonoBehaviour 
 {
 	bool up = false;
-	void Update()
+	void OnMouseDown()
 	{
-		if (Input.GetMouseButtonDown (0))
-		{
+		up = !up;
 			if(!up)
 			{
 				gameObject.GetComponent<Animation>()["Take 001"].speed = 1.0f; 
 				gameObject.GetComponent<Animation> ().Play ("Take 001");
 				Debug.Log ("Drawbridge Clicked");
-				up = true;
 			}
 			else if(up)
 			{
 				gameObject.GetComponent<Animation>()["Take 001"].speed = - 1.0f;
 				gameObject.GetComponent<Animation> ().Play ("Take 001");
 				Debug.Log ("Drawbridge Clicked");
-				up = false;
 			}
-		}
 	}
 }
